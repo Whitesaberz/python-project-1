ebeaf2de11a3e4c3be6c4950241ee437280abe59
Woody White <woody.white276@gmail.com>
2022-10-11 12:41:19 -0400

    part 1 finished

---
+++ part-2/part_2.py
@@ -0,0 +1,77 @@
+### Step 1 - Lists ###
+
+# Fill in this list with several authors you are a fan of. At least 7 or 8 should do.
+my_authors = ['J. R. R. Tolkien', 'George R. R. Martin', 'Stephen King', 'Mark Twain', 'George Orwell', 'Jane Austen', 'Agatha Christie','Shakespeare' ]
+
+# Now let's add a new author to the end with the .append() method. Type your code below.
+
+# Code here
+# Example: my_authors.append("H.G. Wells")
+my_authors.append('H.G. Wells')
+print(my_authors)
+
+
+# Now let's remove an element in the list
+
+# Code here
+# Example: my_authors.remove("H.G. Wells")
+my_authors.remove('Agatha Christie')
+print(my_authors)
+
+# Now access an element by it's index. (Remember it indexes start at 0.)
+
+# Code here
+# Example: my_authors[2]
+print(my_authors[4])
+
+# Now slice the list.
+
+# Code here
+# Example: my_authors[1:4]
+print(my_authors[3:7])
+
+### Step 2 - Tuples ###
+
+# Create your tuple below. Assign it to a variable name you can reference later in the exercise.
+
+# Code here
+# Example: my_author_tuple = ("F. Scott Fitzgerald", "J.K. Rowling", "Ernest Hemingway", "Emily Dickenson", "George Orwell", "Ray Bradbury")
+
+
+### Step 3 - Sets ###
+
+# Create a set with your authors.
+
+# Code here
+# Example: my_author_set = {"F. Scott Fitzgerald", "J.K. Rowling", "Ernest Hemingway", "Emily Dickenson", "George Orwell", "Ray Bradbury"}
+
+
+# Add a new author to your set.
+
+# Code here
+# Example: my_author_set.add("J.R.R. Tolkien")
+
+
+# Try adding the same author again, and be sure to print your set.
+
+# Code here
+# Example: my_author_set.add("J.R.R. Tolkien")
+
+
+
+### Step 4 - For Loops ###
+
+# Create a for-loop for each of the data-structures above.
+
+# Code here
+# Example:
+
+# for book in my_authors:
+    # print(book)
+
+# for book in my_authors_tuple:
+    # print(book)
+
+# for book in my_authors_set:
+    # print(book)
+
